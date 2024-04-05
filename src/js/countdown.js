9b9dcd260f25e2fdd85ecb7aaa494a5960f5aa70
roket1428 <meorhan@protonmail.com>
2024-04-06 02:44:58 +0300

    Move inline javascript to seperate files

---
+++ src/js/countdown.js
@@ -0,0 +1,24 @@
+let countDownDate = new Date("May 14, 2024 10:00:00").getTime();
+
+let x = setInterval(function() {
+
+let now = new Date().getTime();
+
+let distance = countDownDate - now;
+
+let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("eventCountdownDay").innerHTML = days.toString().padStart(2,0);
+document.getElementById("eventCountdownHour").innerHTML = hours.toString().padStart(2,0);
+document.getElementById("eventCountdownMinute").innerHTML = minutes.toString().padStart(2,0);
+document.getElementById("eventCountdownSecond").innerHTML = seconds.toString().padStart(2,0);
+
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("eventCountdownTitle").innerHTML = "Etkinlik Başladı!";
+    document.getElementById("eventCountdown").classList.add("hidden");
+}
+}, 1000);
---
+++ src/js/goToTop.js
@@ -0,0 +1,12 @@
+let toTopButton = document.getElementById("to-top-button");
+let scrollDownIndicator = document.getElementById("scroll-down-indicator");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+        scrollDownIndicator.style.opacity = 0;
+    } else {
+        toTopButton.classList.add("hidden");
+        scrollDownIndicator.style.opacity = 1;
+    }
+}
