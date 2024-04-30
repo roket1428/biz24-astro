64072d59cc5769721475c92f7a61639e34bd6137
roket1428 <meorhan@protonmail.com>
2024-05-01 00:29:51 +0300

    Added sign up button and fixed layout inconsistencies
    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://biz24.tubit.org.tr",
+  integrations: [tailwind(), react(), sitemap()]
 });
